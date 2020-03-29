e98dae41348e66f3a17bbf0a769bbad46290216d
Nilo <nilobdrumond@gmail.com>
2020-03-29 19:43:03 -0300

    Testes backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -7,6 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
+
+module.exports = app;
 
 /**
  *  Rota / Recurso
@@ -54,5 +58,3 @@ app.use(routes);
   *     Listar todos os casos
   *     Entrar em contato com a ONG
   */
-
-app.listen(3333);
